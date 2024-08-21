83f03b17733e8f159c360ddbd4266099a6b503de
Debabrato Sikder <debsikdar15@gmail.com>
2024-08-21 14:37:21 +0530

    Add files via upload

---
+++ Week 4/Exercise 7/BookstoreAPI/src/main/java/org/bookstore/entity/Customer.java
@@ -0,0 +1,17 @@
+package org.bookstore.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    private Integer id;
+    private String name;
+    private String email;
+    private String address;
+}
